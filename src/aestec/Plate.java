af4b8383a4309dd0bed7f66a8dff461f22435dbd
Morten Kuhlwein <Morten@Kuhlwein.dk>
2018-07-04 13:14:58 +0200

    gitignore

---
+++ src/aestec/Plate.java
@@ -0,0 +1,28 @@
+package aestec;
+
+import java.awt.*;
+//import java.util.List;
+
+public interface Plate {//extends Iterable<Point> {
+
+    void add(int x, int y,int value);
+
+    //void expand();
+
+    //boolean contains(int x,int y);
+
+    //void set(int x, int y, int value);
+
+    //Integer get(int x, int y);
+
+    //void move();
+
+    //void remove(int x, int y);
+
+    //int size();
+
+    //List<Point> getOverlapWith(Plate plate);
+
+    //int getAge();
+
+}
---
+++ src/aestec/PlateImpl.java
@@ -0,0 +1,14 @@
+package aestec;
+
+public class PlateImpl implements Plate {
+    private int xdim, ydim;
+
+    public PlateImpl(int xdim, int ydim) {
+        this.xdim=xdim; this.ydim=ydim;
+    }
+
+    @Override
+    public void add(int x, int y, int value) {
+
+    }
+}
